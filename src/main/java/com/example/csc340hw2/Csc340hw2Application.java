039f4192c9d566e4d73ae3891f4cbaff95c658c9
kipVaughan <wovaughan@uncg.edu>
2023-01-31 09:44:33 -0500

    Initial commit

---
+++ src/main/java/com/example/csc340hw2/Csc340hw2Application.java
@@ -0,0 +1,13 @@
+package com.example.csc340hw2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Csc340hw2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Csc340hw2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/csc340hw2/Csc340hw2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.csc340hw2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Csc340hw2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
