f9500af597ef9684cdecd205a28380d317ddc7b4
kipVaughan <wovaughan@uncg.edu>
2023-01-31 09:47:30 -0500

    new

--- src/main/java/com/example/csc340hw2/Csc340hw2Application.java
+++ src/main/java/com/example/csc340hw2/Csc340hw2Application.java
@@ -1,13 +1,42 @@
 package com.example.csc340hw2;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class Csc340hw2Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Csc340hw2Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Csc340hw2Application.class, args);
+        ravenPrice();
+        System.exit(0);
+    }
 
+    public static void ravenPrice() {
+        try {
+            String url = "https://api.coinlore.net/api/ticker/?id=32386";
+            RestTemplate restTemplate = new RestTemplate();
+            ObjectMapper mapper = new ObjectMapper();
+
+            String jSonPrice = restTemplate.getForObject(url, String.class);
+            JsonNode root = mapper.readTree(jSonPrice);
+
+            //gets coin name
+            String coinName = root.findValue("name").asText();
+            //gets coin value in USD
+            double coinPrice = root.findValue("price_usd").asDouble();
+            //print vals
+            System.out.println("Coin: " + coinName);
+            System.out.println("Price: " + coinPrice);
+
+        } catch (JsonProcessingException ex) {
+            System.out.println("error in ravenPrice");
+        }
+    }
 }
--- src/test/java/com/example/csc340hw2/Csc340hw2ApplicationTests.java
+++ src/test/java/com/example/csc340hw2/Csc340hw2ApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class Csc340hw2ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
