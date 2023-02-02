378a4f5a5d2b5f363ded7a1c5635c2fe59186709
kipVaughan <wovaughan@uncg.edu>
2023-02-02 10:32:41 -0500

    removed unused imports

--- src/main/java/com/example/csc340hw2/Csc340hw2Application.java
+++ src/main/java/com/example/csc340hw2/Csc340hw2Application.java
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
